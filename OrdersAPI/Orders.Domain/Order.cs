4ee230a091f8ab1ffa00f661d6dad063255d3c75
Carlos Rodrigues <carlosheadst@gmail.com>
2021-04-12 19:40:48 -0300

    Initial commit

---
+++ OrdersAPI/Orders.Domain/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace Orders.Domain
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public float Price { get; set; }
+    }
+}
